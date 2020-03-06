b5e85042af9e238e0ec3625f2a0d52dbb99f0ef8
buiste <buiste@urbandaleschools.com>
2020-03-06 13:57:49 -0600

    prisoner dilemma

--- 12.0_Jedi_Training.py
+++ 12.0_Jedi_Training.py
@@ -4,25 +4,33 @@ Sign your name:________________
 All questions are about the final code in Chapter 12:
 
 1.) Where is the ball's original location?
+(0,0)
 
 2.) What are the variables dx and dy?
+They are the amount of pixels the ball will move up or down in a second
 
 3.) How many pixels/sec does the ball move in the x-direction?
+dy * 60 will determine the amount
 
 4.) How many pixels/sec does the ball move in the y-direction?
+dx * 60 will determine the amount
 
 5.) Which method is run 60 times/second?
 
 6.) What does this code do?   self.dx *= -1
+make the ball bounce off the border.
 
 7.) What does this code do?  self.pos_y += self.dy
+determine how fast the bal is moving
 
 8.) What is the width of the window?
+640 pixels.
 
 9.) What is this code checking?  self.pos_y > SH - self.rad:
+If the ball has hit the side of the screen
 
 10.) What is this code checking? if self.pos_x < self.rad
-
+if the ball hit the top or bottom of the screen
 
 
 '''
